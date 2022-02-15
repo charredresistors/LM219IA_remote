7fca38c4b32d589fb73164811cced50c28b02b49
charredresistors <charredresitors@gmail.com>
2022-02-15 16:22:34 -0500

    Swapped mute and unmute to be the correct bits.

--- lm219ia_firmware.c
+++ lm219ia_firmware.c
@@ -7,8 +7,8 @@
 /                                                              /
 /   Motor up is defined as P3.0 = 1; P3.1 = 0                  /
 /   Motor down is defined as P3.0 = 0; P3.1 = 1                /
-/   Mute is defined as P1.0 = 0                                /
-/   Unmute is defined as P1.0 = 1                              /
+/   Mute is defined as P1.0 = 1                                /
+/   Unmute is defined as P1.0 = 0                              /
 /   LED on is defined as P3.7 = 0                              /
 /   LED off is defined as P3.7 = 1                             /
 /-------------------------------------------------------------*/
@@ -17,7 +17,9 @@
 #include <mcs51reg.h>
 #include "delay.h"
 
-// Blink LED at approx 1 sec interval, slightly > 30s
+// Blink LED at approx 1 sec interval
+// It says delay_sec(3), but for some reason it works
+// out to just a bit > 1 sec
 void blink(void){
     P3_7 = 1;
     delay_sec(3);
@@ -29,12 +31,12 @@ void blink(void){
 void main(void)
 {
     while(1) {
-        P1_0 = 0; // Mute - startup requres >30s mute
+        P1_0 = 1; // Mute - startup requres >30s mute
         int i;
         for(i=0; i<30; i++){ // Blink LED while muted
-            blink();
+            blink();         // 30 total times for ~30 sec
         }
-        P1_0 = 1; // Unmute for normal amp funtion
+        P1_0 = 0; // Unmute for normal amp funtion
         for(;;){} //Wait forEVER
     }
 }
