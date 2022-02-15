94bffed38ed761aab520a97372ee21b4497bfc34
charredresistors <charredresitors@gmail.com>
2022-02-15 11:52:58 -0500

    First build that works, LED blinks and mute and unmute work.

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 .PRECIOUS: %.ihx
 
-all: LED_test.hex
+all: lm219ia_firmware.hex
 
 %.hex: %.ihx
 	packihx $< > $@
 
 %.ihx: %.c
-	sdcc -mmcs51 --code-size 2048 $< -o $@
+	sdcc -mmcs51 --code-size 2048 --iram-size 2048 $< -o $@
 
 clean:
 	rm -rf *.ihx *.lk *.lst *.map *.mem *.rel *.rst *.sym *.asm
--- lm219ia_firmware.c
+++ lm219ia_firmware.c
@@ -1,10 +1,21 @@
 /*-------------------------------------------------------------/
 /                   Line Magnetic  LM-219IA                    /
 /                   Remote Control Firmware                    /
+/                                                              /
+/   Basic firmware for the LM-219IA receiver board using an    /
+/   AT89C2051 and the SDCC pipeline.                           /
+/                                                              /
+/   Motor up is defined as P3.0 = 1; P3.1 = 0                  /
+/   Motor down is defined as P3.0 = 0; P3.1 = 1                /
+/   Mute is defined as P1.0 = 0                                /
+/   Unmute is defined as P1.0 = 1                              /
+/   LED on is defined as P3.7 = 0                              /
+/   LED off is defined as P3.7 = 1                             /
 /-------------------------------------------------------------*/
+
 #define MICROCONTROLLER_AT89CX051
 #include <mcs51reg.h>
-#include <delay.h>
+#include "delay.h"
 
 // Blink LED at a 1 sec interval
 void blink(void){
@@ -14,25 +25,22 @@ void blink(void){
     delay_sec(1);
 }
 
-// Mute
-void mute(void){
-    P1_0 = 0;
-}
+// Mute the system and blink the LED
 
-//Un-mute
-void unmute(void){
-    P1_0 = 1;
+void mute(void){
+    P1_0 = 0; // Mute
+    int i;
+    for(i=0; i<30; i++){
+        blink();
+    }
+    P1_0 = 1; // Unmute
 }
 
 // Main Loop
 void main(void)
 {
     while(1) {
-        mute();  //Beginning of startup routine
-        int i;
-        for(i=0; i<30; i++)
-            blink();
-        unmute(); //End of startup routine
+        mute(); // Startup requres that the system be muted
         for(;;){} //Wait forEVER
     }
 }
